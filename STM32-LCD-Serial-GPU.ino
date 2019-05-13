3697c1d2acf014120c7f450724673d5f54ebe30c
WacKEDmaN <35508035+WacKEDmaN@users.noreply.github.com>
2019-05-14 04:23:34 +1000

    Add files via upload

--- STM32-LCD-Serial-GPU.ino
+++ STM32-LCD-Serial-GPU.ino
@@ -16,6 +16,26 @@
 // Initialize the data parser using the start, end and delimiting character
 DynamicCommandParser dcp('^', '$', ',');
 
+  // Parser for mandelbrot
+void mandelbrotParser(char **values, int valueCount)
+{
+  if(valueCount != 1) {
+    Serial.println("Error: mandelbrotParser does not like variables");
+  }
+  else
+    {
+      unsigned long t = millis();
+      unsigned long mandeltime = t;
+      Serial.println("mandelbrotParser executing...");
+      drawMandelbrot();  // ^mandelbrot$
+      Serial.println("mandelbrot: Done!");
+      Serial.print(F("Mandelbrot Time = "));
+      mandeltime = (millis() - t)/1000;
+      Serial.print(mandeltime);
+      Serial.println(F(" seconds"));
+    }
+}
+
   // Parser for drawRect,x0,y0,w,h,decimal_color
 void drawRectParser(char **values, int valueCount)
   {
@@ -63,10 +83,7 @@ void fillRectParser(char **values, int valueCount)
   // Parser for fillScreen,decimal_color
 void fillScreenParser(char **values, int valueCount)
   {
-    if(valueCount > 2) {
-      Serial.println("Error: fillScreenParser needs one variable");
-    }
-    if (valueCount < 2) {
+    if(valueCount != 2) {
       Serial.println("Error: fillScreenParser needs one variable");
     }
     else 
@@ -171,9 +188,9 @@ void fillTriangleParser(char **values, int valueCount)
   //Parser for invertDiaplsy,bool_inv
 void invertDisplayParser(char **values, int valueCount)
   {
-   if(valueCount >2)
-    Serial.println("Error: invertDisplayParser: boolean, 0 or 1!");
-   if(valueCount <2)
+   if(valueCount > 2)
+    Serial.println("Error: invertDisplayParser: too many variables!");
+   if(valueCount < 2)
     Serial.println("Error: invertDisplayParser: not enough variables!");
    else
     {
@@ -229,27 +246,167 @@ void fillRoundRectParser(char **values, int valueCount)
     Serial.println("fillRoundRect: Done!");
    }
   }
-
-  // Parser for mandelbrot
-void mandelbrotParser(char **values, int valueCount)
-{
-  if(valueCount != 1) {
-    Serial.println("Error: mandelbrotParser does not like variables");
-  }
-  else
-    {
-      unsigned long t = millis();
-      unsigned long mandeltime = t;
-      Serial.println("mandelbrotParser executing...");
-      drawMandelbrot();  // ^mandelbrot$
-      Serial.println("mandelbrot: Done!");
-      Serial.print(F("Mandelbrot Time = "));
-      mandeltime = (millis() - t)/1000;
-      Serial.print(mandeltime);
-      Serial.println(F(" seconds"));
+  
+  // Parser for setTextColor,color,background_color
+void setTextColorParser(char **values, int valueCount)
+  {
+    if(valueCount > 3) {
+      Serial.println("Error: setTextColorParser: too many variables");
     }
-}
+    if (valueCount < 3) {
+      Serial.println("Error: setTextColorParser: not enough variables");
+    }
+    else 
+    {
+    int color = String(values[1]).toInt();
+    int bgcolor = String(values[2]).toInt();
+    Serial.println("setTextColorParser values: color:" + String(color) + "bgcolor:" + String(bgcolor));
+    tft.setTextColor(color,bgcolor);  // ^setTextColor,1024$
+    Serial.println("setTextColor: Done!");
+    }
+  }
+  
+  // Parser for print,string
+void printParser(char **values, int valueCount)
+  {
+    if(valueCount != 2) {
+      Serial.println("Error: printParser needs one string");
+    }
+    else 
+    {
+    String str = String(values[1]);
+    Serial.println("printParser values: str:" + str);
+    tft.print(str);  // ^print,hello world!$
+    Serial.println("print: Done!");
+    }
+  }
+  
+  // Parser for println,string
+void printlnParser(char **values, int valueCount)
+  {
+    if(valueCount != 2) {
+      Serial.println("Error: printlnParser needs one string");
+    }
+    else 
+    {
+    String str = String(values[1]);
+    Serial.println("printlnParser values: str:" + str);
+    tft.println(str);  // ^println,hello world!$
+    Serial.println("println: Done!");
+    }
+  }
 
+  // Parser for setCursor,col,row;
+void setCursorParser(char **values, int valueCount)
+  {
+    if(valueCount > 3) {
+      Serial.println("Error: setCursorParser too many variables!");
+    }
+    if(valueCount < 3) {
+    Serial.println("Error: setCursorParser: not enough variables!");
+    }
+    else
+    {
+    int col = String(values[1]).toInt();
+    int row = String(values[2]).toInt();
+    Serial.println("setCursorParser values: col:" + String(col) + " row:" + String(row));
+    tft.setCursor(col,row);  // ^setCursor,10,10$
+    Serial.println("setCursor: Done!");
+    }
+  }
+  
+  //Parser for setTextSize,size
+void setTextSizeParser(char **values, int valueCount)
+  {
+   if(valueCount != 2) {
+    Serial.println("Error: setTextSizeParser: needs one variables!");
+   }
+   else
+    {
+    int s = String(values[1]).toInt();
+    Serial.println("setTextSizeParser values: s: " + String(s));
+    tft.setTextSize(s);  //  ^setTextSize,2$
+    Serial.println("setTextSize: Done!");
+    }
+  }
+
+    //Parser for setRotation,0-3
+void setRotationParser(char **values, int valueCount)
+  {
+   if(valueCount != 2) {
+    Serial.println("Error: setRotationParser: needs one variables!");
+   }
+   else
+    {
+    int s = String(values[1]).toInt();
+    Serial.println("setRotationParser values: s: " + String(s));
+    tft.setRotation(s);  //  ^setRotation,0$
+    Serial.println("setRotation: Done!");
+    }
+  }
+
+  // Parser for drawPixel,x0,y0,color
+void drawPixelParser(char **values, int valueCount)
+  {
+   if(valueCount > 4) {
+    Serial.println("Error: drawPixelParser: too many variables!");
+   }
+   if(valueCount < 4) {
+    Serial.println("Error: drawPixelParser: not enough variables!");
+   }
+   else
+    {
+    int x0 = String(values[1]).toInt();
+    int y0 = String(values[2]).toInt();
+    int color = String(values[3]).toInt();
+    Serial.println("drawPixelParser values: x0:" + String(x0) + ", y0:" + String(y0) +  ", color:" + String(color));
+    tft.drawPixel(x0, y0, color); // ^drawPixel,160,120,65535$
+    Serial.println("drawPixel: Done!");
+    }
+  }
+  
+  // Parser for readPixel,x0,y0
+void readPixelParser(char **values, int valueCount)
+  {
+   if(valueCount > 3) {
+    Serial.println("Error: readPixelParser: too many variables!");
+   }
+   if(valueCount < 3) {
+    Serial.println("Error: readPixelParser: not enough variables!");
+   }
+   else
+    {
+    int x0 = String(values[1]).toInt();
+    int y0 = String(values[2]).toInt();
+    Serial.println("readPixelParser values: x0:" + String(x0) + ", y0:" + String(y0));
+    int pixel = tft.readPixel(x0, y0); // ^readPixel,160,120$
+    Serial.println("readPixel: color = " + String(pixel));
+    Serial.println("readPixel: Done!");
+    }
+  }
+  
+  // Parser for drawLine,x0,y0,x1,y1,color
+void drawLineParser(char **values, int valueCount)
+  {
+   if(valueCount > 6) {
+    Serial.println("Error: drawLineParser: too many variables!");
+   }
+   if(valueCount < 6) {
+    Serial.println("Error: drawLineParser: not enough variables!");
+   }
+   else
+   {
+    int x0 = String(values[1]).toInt();
+    int y0 = String(values[2]).toInt();
+    int x1 = String(values[3]).toInt();
+    int y1 = String(values[4]).toInt();
+    int color = String(values[5]).toInt();
+    Serial.println("drawLinwParser values: x0:" + String(x0) + ", y0:" + String(y0) + ", x1:" + String(x1) + ", y1:" + String(y1) + ", color:" + String(color));
+    tft.drawLine(x0, y0, x1, y1, color); //^drawLine,10,10,300,200,65535$
+    Serial.println("drawLine: Done!");
+   }
+  }
+  
 void setup() {
   Serial.begin(115200);  
   tft.reset();
@@ -285,6 +442,15 @@ void setup() {
   dcp.addParser("invertDisplay", invertDisplayParser);
   dcp.addParser("drawRoundRect", drawRoundRectParser);
   dcp.addParser("fillRoundRect", fillRoundRectParser);
+  dcp.addParser("setTextColor", setTextColorParser);
+  dcp.addParser("print", printParser);
+  dcp.addParser("println", printlnParser);
+  dcp.addParser("setCursor", setCursorParser);
+  dcp.addParser("setTextSize", setTextSizeParser);
+  dcp.addParser("setRotation", setRotationParser);
+  dcp.addParser("drawPixel", drawPixelParser);
+  dcp.addParser("readPixel", readPixelParser);
+  dcp.addParser("drawLine", drawLineParser);
   Serial.println("^READY$");
 }
 
